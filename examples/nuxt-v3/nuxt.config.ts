1f662186559a296c83c7619973ed43e61ecd6abd
Jean P.D. Meijer <jean@analoginterface.io>
2025-08-05 13:51:02 +0200

    fix: github actions

--- examples/nuxt-v3/nuxt.config.ts
+++ examples/nuxt-v3/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@simpleanalytics/nuxt"],
+  // modules: ["@simpleanalytics/nuxt"],
   simpleAnalytics: {
     hostname: "test.example.com", // Replace with your actual domain
   },
